dbcacafb8c7881975e197e11a7651122b663eeb3
Jose Hernandez <josehernandez@loandepot.com>
2025-12-22 20:34:47 +0000

    fix bad reference

--- models/marts/core/fact_lead_lifecycle.sql
+++ models/marts/core/fact_lead_lifecycle.sql
@@ -18,7 +18,7 @@ select
   f.stage_date,
   f.lead_source_id
 from {{ ref('stg_fact_lead_lifecycle') }} f
-left join {{ ref('dim_lead_mart') }}        dlead  on f.lead_id        = dlead.lead_id
+left join {{ ref('dim_lead') }}        dlead  on f.lead_id        = dlead.lead_id
 left join {{ ref('dim_associate') }}   dassoc on f.associate_id   = dassoc.associate_id
 left join {{ ref('dim_lead_source') }} dsrc   on f.lead_source_id = dsrc.lead_source_id
 {% if is_incremental() %}
