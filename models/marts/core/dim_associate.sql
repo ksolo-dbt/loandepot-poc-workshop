2d3dced79da0b4192d870e9ac12c3ea2de961546
Jose Hernandez <josehernandez@loandepot.com>
2025-12-22 20:29:15 +0000

    fixes

--- models/marts/core/dim_associate.sql
+++ models/marts/core/dim_associate.sql
@@ -1,7 +1,12 @@
 
 select
-  {{ dbt_utils.generate_surrogate_key(['associate_id','dbt_valid_from']) }} as dim_associate_sk,
-  associate_id, associate_name, role, region, hire_date,
-  dbt_valid_from, dbt_valid_to,
-  case when dbt_valid_to is null then true else false end as is_current
+  {{ dbt_utils.generate_surrogate_key(['associate_id','dbt_valid_from']) }} as dim_associate_sk
+  , associate_id
+  , associate_name
+  , role
+  , region
+  , hire_date
+  , dbt_valid_from
+  , dbt_valid_to
+  , case when dbt_valid_to is null then true else false end as is_current
 from {{ ref('associate_scd2') }}
