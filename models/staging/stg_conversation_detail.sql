72b27f7d07986e3c7350b419d89dba217d080e05
tommy le <tle@loandepot.com>
2025-12-30 00:55:31 +0000

    rename to seed_json_simple

--- models/staging/stg_conversation_detail.sql
+++ models/staging/stg_conversation_detail.sql
@@ -1,10 +1,10 @@
 with source as (
-    select * from {{ ref('raw_app_data') }}
+    select * from {{ ref('seed_json_simple') }}
 )
 
 select
     id,
     json_payload:user_id::int as user_id,
-    json_payload:status::string as status,
-    json_payload:meta.tier::string as tier
+    json_payload:action::string as action,
+    json_payload:timestamp::string as timestamp
 from source
