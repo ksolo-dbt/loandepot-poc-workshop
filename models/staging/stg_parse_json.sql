f16b4bdffc65b519476b859a853d2c8f67dd983a
tommy le <tle@loandepot.com>
2025-12-31 00:51:01 +0000

    Update Json seed

--- models/staging/stg_parse_json.sql
+++ models/staging/stg_parse_json.sql
@@ -3,9 +3,6 @@ with source as (
 )
 
 select
-    payload:user_id::int as user_id,
-    payload:action::string as action,
-    payload:timestamp::string as timestamp,
     payload_nested:companyName::string as company_name,
     payload_nested:location::string as location,
     payload_nested:departments::string as department_detail,
